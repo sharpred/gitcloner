533289999adb0823083ad74b484b6158aa45dc2b
Paul Ryan <paul@sharpred.com>
2023-01-31 12:07:49 +0000

    formatted code

--- bin/main.dart
+++ bin/main.dart
@@ -2,41 +2,37 @@ import 'package:args/args.dart';
 import 'package:git/git.dart';
 import 'package:path/path.dart' as p;
 
-
 Future<void> main(List<String> arguments) async {
   try {
-  var parser = ArgParser();
-parser.addOption('account', abbr:'a', defaultsTo: '');
-parser.addOption('folder', abbr:'f', defaultsTo: '');
-  var results = parser.parse(arguments);
-  String? account = results['account'];
-  String? folder = results['folder'];
-  List<String?> rest = results.rest;
-  if(rest.isEmpty ) {
-    throw Exception("No git repo provided");
-  }
-  String? repo = rest.first;
-  bool? hasAccount = account?.isNotEmpty;
-  bool isGitDir = await GitDir.isGitDir(p.current);
-
-  if(isGitDir==false) {
-    if(repo !=null) {
-      if(hasAccount==true){
-    repo = repo.replaceAll("github.com", "github.com-$account");
-      }
- List<String> gitArguments = ['clone', repo];
-    if(folder != null && folder.isNotEmpty) {
-      gitArguments.add(folder);
+    var parser = ArgParser();
+    parser.addOption('account', abbr: 'a', defaultsTo: '');
+    parser.addOption('folder', abbr: 'f', defaultsTo: '');
+    var results = parser.parse(arguments);
+    String? account = results['account'];
+    String? folder = results['folder'];
+    List<String?> rest = results.rest;
+    if (rest.isEmpty) {
+      throw Exception("No git repo provided");
     }
-    runGit(gitArguments);
-  }
+    String? repo = rest.first;
+    bool? hasAccount = account?.isNotEmpty;
+    bool isGitDir = await GitDir.isGitDir(p.current);
 
-
-  } else {
-    throw Exception("Cannot clone into an existing git folder");
-  }
-
-  } catch(ex) {
+    if (isGitDir == false) {
+      if (repo != null) {
+        if (hasAccount == true) {
+          repo = repo.replaceAll("github.com", "github.com-$account");
+        }
+        List<String> gitArguments = ['clone', repo];
+        if (folder != null && folder.isNotEmpty) {
+          gitArguments.add(folder);
+        }
+        runGit(gitArguments);
+      }
+    } else {
+      throw Exception("Cannot clone into an existing git folder");
+    }
+  } catch (ex) {
     print("error $ex");
   }
 }
